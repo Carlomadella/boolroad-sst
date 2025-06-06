cc29018fe9635b1097aa0b0d36de50c6d29ee7f8
Carlomadella <madella871@gmail.com>
2025-06-06 17:17:02 +0200

    first commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,84 @@
+const viaggi = [
+  {
+    id: "V001",
+    nome: "Esplorazione del Gran Paradiso",
+    data_partenza: "2025-07-15",
+    data_arrivo: "2025-07-21",
+    tipologia: "Avventure wild",
+    luogo: "Valle d'Aosta",
+    coordinatore: "Luca Moretti",
+    accompagnatori: ["Giulia Ferri", "Marco Neri"],
+    partecipanti: 18
+  },
+  {
+    id: "V002",
+    nome: "Tour Culturale in Toscana",
+    data_partenza: "2025-08-03",
+    data_arrivo: "2025-08-08",
+    tipologia: "Visite guidate soft",
+    luogo: "Firenze, Siena, Pisa",
+    coordinatore: "Elena Bianchi",
+    accompagnatori: ["Francesca Rossi"],
+    partecipanti: 20
+  },
+  {
+    id: "V003",
+    nome: "Trekking nelle Dolomiti",
+    data_partenza: "2025-06-22",
+    data_arrivo: "2025-06-28",
+    tipologia: "Avventure wild",
+    luogo: "Val di Fassa, Trentino-Alto Adige",
+    coordinatore: "Davide Conti",
+    accompagnatori: ["Lucia Verdi", "Andrea Gallo"],
+    partecipanti: 17
+  },
+  {
+    id: "V004",
+    nome: "Alla scoperta di Roma Antica",
+    data_partenza: "2025-09-10",
+    data_arrivo: "2025-09-13",
+    tipologia: "Visite guidate soft",
+    luogo: "Roma",
+    coordinatore: "Sara Mancini",
+    accompagnatori: ["Federico Romano"],
+    partecipanti: 19
+  },
+  {
+    id: "V005",
+    nome: "Weekend Selvaggio in Maremma",
+    data_partenza: "2025-06-06",
+    data_arrivo: "2025-06-08",
+    tipologia: "Avventure wild",
+    luogo: "Parco della Maremma, Toscana",
+    coordinatore: "Matteo Ricci",
+    accompagnatori: ["Chiara De Luca"],
+    partecipanti: 16
+  }
+];
+
+
+const Homepage = () => {
+  return (
+    <div className="row mt-2 px-3 gy-3">
+      <div className="col-12 text-end">
+        <h1 className="fw-semibold text-white">Gite in corso</h1>
+      </div>
+      {viaggi.map(elem => (
+        <div className="col-12">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="card-title">{elem.nome}</h3>
+              <h6 className="card-title fw-semibold">{elem.luogo}</h6>
+              <div className="card-text">
+                <i class="fa-regular fa-calendar me-1"></i>{elem.data_partenza}
+                <i class="fa-regular fa-calendar ms-5 me-1"></i>{elem.data_arrivo}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Homepage
